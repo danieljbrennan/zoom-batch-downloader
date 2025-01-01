af35366d385eab0083f70e3562393907f13c8316
daniel <daniel.brennan@draftrs.com>
2025-01-01 10:23:28 -0800

    don't download audio track, verbose output, tolerate size mismatch

--- config.py
+++ config.py
@@ -25,19 +25,19 @@ USERS = [
 # Put here the topics of the meetings you wish to download recordings for. If empty, no topic filtering will happen.
 TOPICS = [
     # R"############",
-    # R"############",
+    # R"############"
 ]
 
 # Put here the file types you wish to download. If empty, no file type filtering will happen.
 RECORDING_FILE_TYPES = [
-    # R"MP4",            # Video file of the recording.
+    R"MP4",            # Video file of the recording.
     # R"M4A",            # Audio-only file of the recording.
-    # R"TIMELINE",       # Timestamp file of the recording in JSON file format.
-    # R"TRANSCRIPT",     # Transcription file of the recording in VTT format.
-    # R"CHAT",           # A TXT file containing in-meeting chat messages that were sent during the meeting.
-    # R"CC",             # File containing closed captions of the recording in VTT file format.
-    # R"CSV",            # File containing polling data in CSV format.
-    # R"SUMMARY",        # Summary file of the recording in JSON file format.
+    R"TIMELINE",       # Timestamp file of the recording in JSON file format.
+    R"TRANSCRIPT",     # Transcription file of the recording in VTT format.
+    R"CHAT",           # A TXT file containing in-meeting chat messages that were sent during the meeting.
+    R"CC",             # File containing closed captions of the recording in VTT file format.
+    R"CSV",            # File containing polling data in CSV format.
+    R"SUMMARY",        # Summary file of the recording in JSON file format.
 ]
 
 # If True, recordings will be grouped in folders by their owning user.
@@ -55,7 +55,7 @@ GROUP_BY_RECORDING = False
 INCLUDE_PARTICIPANT_AUDIO = True
 
 # Set to True for more verbose output.
-VERBOSE_OUTPUT = False
+VERBOSE_OUTPUT = True
 
 # Constants used for indicating size in bytes.
 B = 1
@@ -72,4 +72,4 @@ MINIMUM_FREE_DISK = 1 * GB
 # actually downloaded from the server.
 # This was observed to happen sometimes on google drive mounted storage (mismatches of < 300 KBs).
 # Note: High tolerance might cause issues like corrupt downloads not being recognized by script.
-FILE_SIZE_MISMATCH_TOLERANCE = 0 * KB
+FILE_SIZE_MISMATCH_TOLERANCE = 100 * KB
